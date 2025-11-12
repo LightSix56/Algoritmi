0e733f8a9ec1894b6ae27760b25600cfd4ddf6d8
LightSix56 <105928444+LightSix56@users.noreply.github.com>
2025-11-12 13:48:48 +0300

    Create Жадный алгоритм для составления расписания.py

---
+++ Жадный алгоритм для составления расписания.py
@@ -0,0 +1,13 @@
+def greedy_schedule(num_machines, jobs):
+    machine_loads = [0] * num_machines
+    assignment = [0] * len(jobs)
+    for i in range(len(jobs)):
+        min_load = min(machine_loads)
+        min_machine = machine_loads.index(min_load)
+        machine_loads[min_machine] += jobs[i]
+        assignment[i] = min_machine
+    makespan = max(machine_loads)
+    return assignment, makespan
+
+print(greedy_schedule(3, [5, 7, 3, 8, 2, 6, 4, 9, 1, 5])
+#выводит пример работы
